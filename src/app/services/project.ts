f1c694364c9ba0976e267544f6e07fe0c98298bc
maaz2692 <maazmustafa2692@gmail.com>
2025-11-23 15:06:19 +0100

    get builders functinality

--- src/app/services/project.ts
+++ src/app/services/project.ts
@@ -2,18 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ProjectsModel } from '../models/Projects.model';
+import { Developer } from '../models/developers.model';
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
   private jsonUrl = '/projects.json';
   private MlsUrl = '/mlszone.json';
+  private developerUrl = '/developers.json'
 
   constructor(private http: HttpClient) { }
   private searchResult: any
 
   getProjects(): Observable<ProjectsModel[]> {
     return this.http.get<ProjectsModel[]>(this.jsonUrl);
+  }
+
+  getBuilders(): Observable<Developer[]>{
+    return this.http.get<Developer[]>(this.developerUrl)
   }
 
   getMlsZone(): Observable<{ id: number; name: string; }[]> {
