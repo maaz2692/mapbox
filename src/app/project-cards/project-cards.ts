749fda0156946d807c395f38237d3f39d565bae6
maaz2692 <maazmustafa2692@gmail.com>
2025-12-01 12:56:19 +0100

    final touches

--- src/app/project-cards/project-cards.ts
+++ src/app/project-cards/project-cards.ts
@@ -24,38 +24,6 @@ export class ProjectCards implements OnInit {
 
   @Input() project!: ProjectsModel;
   ngOnInit() {
-    console.log(this.project.project_stub)
-    const storedProjects = localStorage.getItem(this.localStorageKey);
-    if (storedProjects) {
-      if (storedProjects.includes(this.project.project_stub ?? "")) {
-        this.fav = true;
-      }
-    }
-  }
 
-  FavClicked(event: Event): void {
-    event.stopPropagation();
-    const storedProjectsString = localStorage.getItem(this.localStorageKey);
-
-    // Parse the storedProjects string into an array, or use an empty array if it is null
-    const storedProjects = storedProjectsString
-      ? JSON.parse(storedProjectsString)
-      : [];
-
-    const index = storedProjects.indexOf(this.project.project_stub);
-    if (index === -1) {
-      this.fav = true;
-      storedProjects.push(this.project.project_stub);
-    } else {
-      this.fav = false;
-      storedProjects.splice(index, 1);
-    }
-
-    // Store the updated array back to localStorage
-    localStorage.setItem(this.localStorageKey, JSON.stringify(storedProjects));
-  }
-
-  private getUserInfoFromLocalStorage() {
-    return localStorage.getItem('user-mo');
   }
 }
